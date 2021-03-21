b60d7689ef6a286b087ea01e4acf4da6f1a9480a
Mizuki Tanabe <leftycamel18@gmail.com>
2021-03-22 00:43:53 +0900

    パスワード迷走

--- necocchi.py
+++ necocchi.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, session, url_for
+from flask import Flask, render_template, request, redirect, session, url_for, flash, abort
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask_mail import Mail, Message
 import os
+import secrets
 import sqlite3
 import smtplib
 # from flask_mail import Mail, Message 
@@ -187,7 +188,7 @@ def verify_reset_token(token):
 
 @app.route('/reset_password', methods=['GET', 'POST'])
 def reset_request():
-    if current_user.is_authenticated:
+    if user_email_address.is_authenticated:
         return redirect(url_for('home'))
     form = RequestResetForm()
     if form.validate_on_submit():
@@ -199,7 +200,7 @@ def reset_request():
 
 @app.route('/reset_password/<token>', methods=['GET', 'POST'])
 def reset_token(token):
-    if current_user.is_authenticated:
+    if user_email_address.is_authenticated:
         return redirect(url_for('home'))
     user = User.verify_reset_token(token)
     if user is None:
